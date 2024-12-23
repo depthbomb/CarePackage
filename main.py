c2e0990d49adad7ba633c958a611c519b3fb9f10
depthbomb <depthbomb@users.noreply.github.com>
2024-12-22 19:06:27 -0600

    refactor: don't set default settings for onefile builds

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sys import argv, exit
 from PySide6.QtWidgets import QApplication
 from src.windows.main_window import MainWindow
 from src.windows.disclaimer_window import DisclaimerWindow
-from src import APP_ORG, APP_NAME, DOWNLOAD_DIR, APP_DISPLAY_NAME, APP_VERSION_STRING
+from src import APP_ORG, APP_NAME, IS_ONEFILE, DOWNLOAD_DIR, APP_DISPLAY_NAME, APP_VERSION_STRING
 from src.lib.settings import app_settings, user_settings, DownloadTimeout, AppSettingsKeys, UserSettingsKeys
 
 argv += ['-platform', 'windows:darkmode=0']
@@ -22,13 +22,16 @@ if __name__ == '__main__':
         else:
             app_settings.setValue(AppSettingsKeys.SeenDisclaimer, True)
 
-    if not user_settings.value(UserSettingsKeys.DownloadTimeout, None, int):
-        user_settings.setValue(UserSettingsKeys.DownloadTimeout, DownloadTimeout.ThreeMinutes.value)
+    if not IS_ONEFILE:
+        if not user_settings.value(UserSettingsKeys.DownloadTimeout, None, int):
+            user_settings.setValue(UserSettingsKeys.DownloadTimeout, DownloadTimeout.ThreeMinutes.value)
 
-    if user_settings.value(UserSettingsKeys.Theme, None) is None:
-        user_settings.setValue(UserSettingsKeys.Theme, True)
+        if user_settings.value(UserSettingsKeys.Theme, None) is None:
+            user_settings.setValue(UserSettingsKeys.Theme, True)
 
-    if user_settings.value(UserSettingsKeys.Theme, None, bool):
+        if user_settings.value(UserSettingsKeys.Theme, None, bool):
+            app.setStyle('Fusion')
+    else:
         app.setStyle('Fusion')
 
     w = MainWindow()

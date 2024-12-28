7c6306b90d2f045830bfcb4cbec66d16aa181014
depthbomb <depthbomb@users.noreply.github.com>
2024-12-28 03:02:45 -0600

    docs: add more info to SOFTWARE.md

--- scripts/generate_software_table.py
+++ scripts/generate_software_table.py
@@ -2,8 +2,8 @@ from pathlib import Path
 from src import SOFTWARE_CATALOGUE
 
 markdown_file = Path('.') / '..' / 'SOFTWARE.md'
-if not markdown_file.exists():
-    markdown_file.touch()
+if markdown_file.exists():
+    markdown_file.unlink()
 
 markdown = '# All Software'
 markdown += '\n\nThis file was generated from `scripts/generate_software_table.py`'
@@ -11,11 +11,13 @@ markdown += '\n\n---'
 
 for (category, software_list) in SOFTWARE_CATALOGUE.items():
     markdown += f'\n\n## {category}'
-    markdown += '\n\n|  | Name | Homepage |'
-    markdown += '\n| ---- | ---- | -------- |'
+    markdown += '\n\n|  | Name | Homepage | Is Archive? | Requires Admin? |'
+    markdown += '\n| :-: | --- | --- | :-: | :-: |'
     for software in software_list:
         icon_path = f'./resources/images/software/{software.icon}'
-        markdown += f'\n| ![{software.name}]({icon_path} "{software.name}") | {software.name} | [{software.homepage}]({software.homepage}) |'
+        is_archive = '✔️' if software.is_archive else '❌'
+        requires_admin = '✔️' if software.requires_admin else '❌'
+        markdown += f'\n| ![{software.name}]({icon_path} "{software.name}") | {software.name} | [{software.homepage}]({software.homepage}) | {is_archive} | {requires_admin} |'
     markdown += '\n\n---'
 
 with markdown_file.open('w') as f:

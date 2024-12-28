4472c0d7528337024bdcc19c72521ca075888629
depthbomb <depthbomb@users.noreply.github.com>
2024-12-28 16:59:25 -0600

    docs: make software name link to homepage

--- scripts/generate_software_table.py
+++ scripts/generate_software_table.py
@@ -11,14 +11,14 @@ markdown += '\n\n---'
 
 for (category, software_list) in SOFTWARE_CATALOGUE.items():
     markdown += f'\n\n## {category}'
-    markdown += '\n\n|  | Name | Homepage | Is Archive? | Requires Admin? |'
-    markdown += '\n| :-: | --- | --- | :-: | :-: |'
+    markdown += '\n\n|  | Name | Is Archive? | Requires Admin? |'
+    markdown += '\n| :-: | --- | :-: | :-: |'
     for software in software_list:
         icon_path = f'./resources/images/software/{software.icon}'
         is_archive = '✔️' if software.is_archive else '❌'
         requires_admin = '✔️' if software.requires_admin else '❌'
         homepage_text = software.homepage if len(software.homepage) < 47 else software.homepage[:47] + '...'
-        markdown += f'\n| ![{software.name}]({icon_path} "{software.name}") | {software.name} | [{homepage_text}]({software.homepage}) | {is_archive} | {requires_admin} |'
+        markdown += f'\n| ![{software.name}]({icon_path} "{software.name}") | [{software.name}]({software.homepage}) | {is_archive} | {requires_admin} |'
     markdown += '\n\n---'
 
 with markdown_file.open('w') as f:

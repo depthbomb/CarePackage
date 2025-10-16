361d0bb3532747d92b6c48fcd120239142b0fd6c
depthbomb <depthbomb@users.noreply.github.com>
2025-10-16 13:44:11 -0500

    fix: update download URL resolving for LibreOffice

--- src/lib/software/libreoffice.py
+++ src/lib/software/libreoffice.py
@@ -16,7 +16,7 @@ class LibreOffice(BaseSoftware):
         self.icon = 'libreoffice.png'
         self.homepage = 'https://libreoffice.org'
 
-        self._initial_url = QUrl('https://libreoffice.org/download/download-libreoffice')
+        self._initial_url = QUrl('https://www.libreoffice.org/download/download-libreoffice/')
 
     @Slot(QNetworkReply)
     def on_manager_finished(self, reply: QNetworkReply):
@@ -26,19 +26,14 @@ class LibreOffice(BaseSoftware):
             self.url_resolve_error.emit(self.ResolveError.URLResolveError)
             return
 
-        if reply.url() == self._initial_url:
-            url = reply.url().toString()
-            url += 'windows/64-bit/exe/dl'
-            self.manager.get(QNetworkRequest(url))
+        pattern = compile(r'(\d+\.\d+\.\d+)_Win_x86_helppack')
+        html = reply.readAll().data().decode()
+        match = pattern.search(html)
+        if not match:
+            self.url_resolve_error.emit(self.ResolveError.URLResolveError)
         else:
-            pattern = compile(r'href=\'/download/download-libreoffice/\?type=win-x86_64&version=(\d+\.\d+\.\d)+&lang=en-US\'><i class=".*"></i>Windows x86_64 \(Windows 7 or newer required\)')
-            html = reply.readAll().data().decode()
-            match = pattern.search(html)
-            if not match:
-                self.url_resolve_error.emit(self.ResolveError.URLResolveError)
-            else:
-                version = match.group(1)
-                self.url_resolved.emit(f'https://mirror.usi.edu/pub/tdf/libreoffice/stable/{version}/win/x86_64/LibreOffice_{version}_Win_x86-64.msi')
+            version = match.group(1)
+            self.url_resolved.emit(f'https://mirror.fcix.net/tdf/libreoffice/stable/{version}/win/x86_64/LibreOffice_{version}_Win_x86-64.msi')
 
     def resolve_download_url(self):
         self.manager.get(QNetworkRequest(self._initial_url))

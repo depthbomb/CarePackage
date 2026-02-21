9580ea2725b82de778230e44fe64f99312bbc613
depthbomb <depthbomb@users.noreply.github.com>
2026-02-21 12:05:54 -0600

    refactor: replace _Visual Studio 2022 Community_ with _Visual Studio 2026 Community_

--- src/lib/software/visual_studio_community.py
+++ src/lib/software/visual_studio_community.py
@@ -1,35 +1,16 @@
-from re import compile
-from PySide6.QtCore import Slot
 from src.lib.software import BaseSoftware, SoftwareCategory
-from PySide6.QtNetwork import QNetworkReply, QNetworkRequest
 
 class VisualStudioCommunity(BaseSoftware):
     def __init__(self):
         super().__init__()
 
         self.key = 'visual-studio-community'
-        self.name = 'Visual Studio 2022 Community'
+        self.name = 'Visual Studio 2026 Community'
         self.category = [SoftwareCategory.Development]
         self.download_name = 'VisualStudioSetup.exe'
         self.should_cache_url = True
         self.icon = 'visual-studio-community.png'
         self.homepage = 'https://visualstudio.microsoft.com'
 
-    @Slot(QNetworkReply)
-    def on_manager_finished(self, reply: QNetworkReply):
-        reply.deleteLater()
-        error = reply.error()
-        if error != QNetworkReply.NetworkError.NoError:
-            self.url_resolve_error.emit(self.ResolveError.URLResolveError)
-            return
-
-        download_url_pattern = compile(r'https://c2rsetup\.officeapps\.live\.com/c2r/downloadVS\.aspx\?sku=community&channel=Release&version=VS\d{4}&passive=true')
-        html = reply.readAll().data().decode()
-        match = download_url_pattern.search(html)
-        if not match:
-            self.url_resolve_error.emit(self.ResolveError.URLResolveError)
-        else:
-            self.url_resolved.emit(match.group(0))
-
     def resolve_download_url(self):
-        self.manager.get(QNetworkRequest('https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=Community&channel=Release'))
+        self.url_resolved.emit('https://c2rsetup.officeapps.live.com/c2r/downloadVS.aspx?sku=community&channel=stable&version=VS18&source=VSLandingPage')

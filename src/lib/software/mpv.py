6e5bf4bbda26734ac92f4f9fe7aab3156da53cd8
depthbomb <depthbomb@users.noreply.github.com>
2026-02-21 12:16:20 -0600

    refactor: update download URL resolving for _mpv_

--- src/lib/software/mpv.py
+++ src/lib/software/mpv.py
@@ -1,8 +1,13 @@
+from PySide6.QtCore import Slot
 from src.lib.software import BaseSoftware, SoftwareCategory
+from src.lib.github_release_scraper import GithubReleaseScraper
 
 class MPV(BaseSoftware):
     def __init__(self):
         super().__init__()
+
+        self._gh = GithubReleaseScraper('mpv-player', 'mpv', self)
+        self._gh.releases_scraped.connect(self._on_releases_scraped)
 
         self.key = 'mpv'
         self.name = 'mpv'
@@ -12,5 +17,13 @@ class MPV(BaseSoftware):
         self.icon = 'mpv.png'
         self.homepage = 'https://mpv.io'
 
+    @Slot(list)
+    def _on_releases_scraped(self, releases: list[str]):
+        asset = next((release for release in releases if release.endswith('x86_64-pc-windows-msvc.zip')), None)
+        if asset:
+            self.url_resolved.emit(asset)
+        else:
+            self.url_resolve_error.emit(self.ResolveError.GitHubAssetNotFoundError)
+
     def resolve_download_url(self):
-        self.url_resolved.emit('https://nightly.link/mpv-player/mpv/workflows/build/master/mpv-x86_64-pc-windows-msvc.zip')
+        self._gh.get_repo_releases()

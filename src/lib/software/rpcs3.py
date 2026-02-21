1801b346731845e690e3503658f9c261b5763813
depthbomb <depthbomb@users.noreply.github.com>
2026-02-21 11:48:26 -0600

    refactor: update download URL resolving for _RPCS3_

--- src/lib/software/rpcs3.py
+++ src/lib/software/rpcs3.py
@@ -1,37 +1,30 @@
-from re import compile
 from PySide6.QtCore import Slot
 from src.lib.software import BaseSoftware, SoftwareCategory
-from PySide6.QtNetwork import QNetworkReply, QNetworkRequest
+from src.lib.github_release_scraper import GithubReleaseScraper
 
 class RPCS3(BaseSoftware):
     def __init__(self):
         super().__init__()
 
+        self._gh = GithubReleaseScraper('RPCS3', 'rpcs3-binaries-win', self)
+        self._gh.releases_scraped.connect(self._on_releases_scraped)
+
         self.key = 'rpcs3'
         self.name = 'RPCS3'
         self.category = [SoftwareCategory.Emulation, SoftwareCategory.Gaming]
-        self.download_name = 'rpcs3-build-win64.7z'
+        self.download_name = 'rpcs3-win64_msvc.7z'
         self.is_archive = True
         self.should_cache_url = True
         self.icon = 'rpcs3.png'
         self.homepage = 'https://rpcs3.net'
 
-    @Slot(QNetworkReply)
-    def on_manager_finished(self, reply: QNetworkReply):
-        reply.deleteLater()
-        error = reply.error()
-        if error != QNetworkReply.NetworkError.NoError:
-            self.url_resolve_error.emit(self.ResolveError.URLResolveError)
-            return
-
-        html = reply.readAll().data().decode()
-
-        pattern = compile(r'https://github.com/RPCS3/rpcs3-binaries-win/releases/download/build-[a-fA-F0-9]{40}/rpcs3-v\d+.\d+.\d+-\d+-[a-fA-F0-9]{8}_win64.7z')
-        match = pattern.search(html)
-        if not match:
-            self.url_resolve_error.emit(self.ResolveError.URLResolveError)
+    @Slot(list)
+    def _on_releases_scraped(self, releases: list[str]):
+        asset = next((release for release in releases if release.endswith('.7z')), None)
+        if asset:
+            self.url_resolved.emit(asset)
         else:
-            self.url_resolved.emit(match.group(0))
+            self.url_resolve_error.emit(self.ResolveError.GitHubAssetNotFoundError)
 
     def resolve_download_url(self):
-        self.manager.get(QNetworkRequest('https://rpcs3.net/download'))
+        self._gh.get_repo_releases()

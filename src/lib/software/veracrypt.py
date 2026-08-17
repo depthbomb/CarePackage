fca432efb3c38804efdd02e86883747f1e707890
depthbomb <depthbomb@users.noreply.github.com>
2026-08-17 00:30:26 -0500

    fix: download URL resolving for _VeraCrypt_

--- src/lib/software/veracrypt.py
+++ src/lib/software/veracrypt.py
@@ -1,35 +1,29 @@
-from re import compile
 from PySide6.QtCore import Slot
 from src.lib.software import BaseSoftware, SoftwareCategory
-from PySide6.QtNetwork import QNetworkReply, QNetworkRequest
+from src.lib.github_release_scraper import GithubReleaseScraper
 
 class VeraCrypt(BaseSoftware):
     def __init__(self):
         super().__init__()
 
+        self._gh = GithubReleaseScraper('veracrypt', 'VeraCrypt', self)
+        self._gh.releases_scraped.connect(self._on_releases_scraped)
+
         self.key = 'veracrypt'
         self.name = 'VeraCrypt'
         self.category = [SoftwareCategory.Security, SoftwareCategory.SystemManagement]
-        self.download_name = 'VeraCrypt_Setup_x64.msi'
+        self.download_name = 'VeraCrypt.Setup.exe'
         self.should_cache_url = True
         self.icon = 'veracrypt.png'
         self.homepage = 'https://veracrypt.io'
 
-    @Slot(QNetworkReply)
-    def on_manager_finished(self, reply: QNetworkReply):
-        reply.deleteLater()
-        error = reply.error()
-        if error != QNetworkReply.NetworkError.NoError:
-            self.url_resolve_error.emit(self.ResolveError.URLResolveError)
-            return
-
-        download_url_pattern = compile(r'https://launchpad\.net/veracrypt/trunk/\d\.\d+\.\d+/\+download/VeraCrypt_Setup_x64_\d\.\d+\.\d+\.msi')
-        html = reply.readAll().data().decode()
-        match = download_url_pattern.search(html)
-        if not match:
-            self.url_resolve_error.emit(self.ResolveError.URLResolveError)
+    @Slot(list)
+    def _on_releases_scraped(self, releases: list[str]):
+        asset = next((release for release in releases if '.Setup.' in release and release.endswith('.exe')), None)
+        if asset:
+            self.url_resolved.emit(asset)
         else:
-            self.url_resolved.emit(match.group(0))
+            self.url_resolve_error.emit(self.ResolveError.GitHubAssetNotFoundError)
 
     def resolve_download_url(self):
-        self.manager.get(QNetworkRequest('https://veracrypt.io/en/Downloads.html'))
+        self._gh.get_repo_releases()
